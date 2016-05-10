d6bf06b30df93373eac396b21d7eeba543a0db41
Thomas Langnickel <Langnickel.thomas@gmail.com>
2016-05-11 09:31:01 +1000

    selection of avatar works

--- js/TicTac.js
+++ js/TicTac.js
@@ -4,6 +4,9 @@
 var avatar = $('.avatars img');
 var avatarOne = $('.avatars #avatarOne');
 var avatarTwo = $('.avatars #avatarTwo');
+// var playerOne = '<img src="images/Clinton.jpg"/>';
+// var playerTwo = '<img src="images/Bernie.jpg"/>';
+// works fine with that
 var playerOne = '';
 var playerTwo = '';
 var reloadButton = $('#reload');
@@ -21,51 +24,48 @@ var maxHeight = 100;
 var maxWidth = 100;
 var playerOneScore = 0;
 var playerTwoScore = 0;
+var winningMessage =['Peace with more firepower!',
+                      'Feel the burn!',
+                      'Kick out the immigrants!',
+                      'WTF!',
+                      'We need more labor camps!',
+                      'I m jamming...',
+                      'Come to the dark side...'];
 
 
-avatar.on('click', function()
-        {
-          if(playerOne === '')
-            {
-              playerOne = this;
-              avatarOne.append(playerOne).css('display', 'flex');
-              console.log(playerOne);
-            }
+  avatar.on('click', function()
+          {
+            if(playerOne === '')
+              {
+                playerOne = this;
+                avatarOne.append(playerOne).css('display', 'flex');
+                console.log(playerOne);
+              }
           else if (playerTwo === '')
-          {
-            playerTwo = this;
-            avatarTwo.append(playerTwo).css('display', 'flex');
-            console.log(playerTwo);
-          }
-          
+            {
+              playerTwo = this;
+              avatarTwo.append(playerTwo).css('display', 'flex');
+              console.log(playerTwo);
+            }
           else
-          {
-            $('.menu').css('display', 'none');
-            $('.game').css('display', 'block');
-          }
+            {
+              $('.menu').css('display', 'none');
+              $('.game').css('display', 'block');
+            }
         });
-
-// $('.menu').hide()
-// $('.game').show()
-
-
 reloadButton.on('click',function(event)
   {
-  location.reload();
+    location.reload();
   });
-
 startButton.on('click',function(event)
 {
   console.log('clicked');
   start();
 });
-
-
 function boardMsg(x)
     {
       return $("#board").text(x);
     }
-
 function start()
 {
             turn = "";
@@ -81,45 +81,40 @@ function start()
         $('#gameField').css('display', 'inline-block');
 }
 
-
-
 $('.column').on('click', function()
 {
   var row = $(this).parent().index();
   var column = $(this).index();
-  function checkWinner(n,playerName)
-  {
-      if(
-          (grid[0][0]==n && grid[0][1]==n && grid[0][2]==n) ||
-          (grid[1][0]==n && grid[1][1]==n && grid[1][2]==n) ||
-          (grid[2][0]==n && grid[2][1]==n && grid[2][2]==n) ||
-
-          (grid[0][0]==n && grid[1][0]==n && grid[2][0]==n) ||
-          (grid[0][1]==n && grid[1][1]==n && grid[2][1]==n) ||
-          (grid[0][2]==n && grid[1][2]==n && grid[2][2]==n) ||
-
-          (grid[0][0]==n && grid[1][1]==n && grid[2][2]==n)||
-          (grid[0][2]==n && grid[1][1]==n && grid[2][0]==n)
-
-          ){
-          boardMsg(playerName + " won the game!").css('color', 'red');
-          winner = 1;
-          turn=0;
-          return true;
+    function checkWinner(n,playerName)
+      {
+        if(
+            (grid[0][0]==n && grid[0][1]==n && grid[0][2]==n) ||
+            (grid[1][0]==n && grid[1][1]==n && grid[1][2]==n) ||
+            (grid[2][0]==n && grid[2][1]==n && grid[2][2]==n) ||
+            (grid[0][0]==n && grid[1][0]==n && grid[2][0]==n) ||
+            (grid[0][1]==n && grid[1][1]==n && grid[2][1]==n) ||
+            (grid[0][2]==n && grid[1][2]==n && grid[2][2]==n) ||
+            (grid[0][0]==n && grid[1][1]==n && grid[2][2]==n)||
+            (grid[0][2]==n && grid[1][1]==n && grid[2][0]==n)
+            )
+        {
+            winner = 1;
+            turn=0;
+            return true;
+        }
+        return false;
       }
-      return false;
-    }
-
-if(grid[row][column] !==0)
+    if(grid[row][column] !==0)
       {
           alert("This position is taken. Please try other position.");
           return;
       }
-
-if (turn%2 === 0)
+    if (turn%2 === 0)
     {
-      currentPlayer = playerOne;
+      currentPlayer = playerOne.src;
+      console.log(currentPlayer);
       grid[row][column] = 1;
+
       win = checkWinner(1,playerOne);
       if(winner !== 0)
         {
@@ -127,45 +122,37 @@ if (turn%2 === 0)
           boardMsg("Peace by more firepower").css({'color': 'red','margin-left' : '20px', 'margin-top' : '0px'});
 
           playerOneScore++;
-          console.log(playerOneScore);
           scoreBoardOne.text('Clinton: ' + playerOneScore)
                     .css({"color": "red",
                           'font-size' : '30px',
                           'font-weight' : 'bold',
                           'display': 'block'});
-
         }
-
     }
-  else
+    else
     {
-      currentPlayer = playerTwo;
+      currentPlayer = playerTwo.src;
+      console.log(currentPlayer);
       grid[row][column] = 2;
+
       win = checkWinner(2,'Player 2');
+        if(winner !== 0)
+          {
+            $('#gameField').css('display', 'none');
+            boardMsg("Feel the Burn!").css({'color': 'red','margin-left' : '20px','margin-top' : '0px'});
 
-
-      if(winner !== 0)
-        {
-          $('#gameField').css('display', 'none');
-          boardMsg("Feel the Burn!").css({'color': 'red','margin-left' : '20px','margin-top' : '0px'});
-
-          playerTwoScore++;
-          console.log(playerTwoScore);
-          scoreBoardTwo.text('Bernie: ' + playerTwoScore);
-
-
-        }
+            playerTwoScore++;
+            scoreBoardTwo.text('Bernie: ' + playerTwoScore);
+          }
     }
   turn++;
 
-    $(this).append(currentPlayer);
-    console
+    $(this).css('background-image', 'url('+currentPlayer+ ')');
 
       if(turn === 9)
       {
-        console.log('Draw');
         $('#gameField').css('display', 'none');
-        boardMsg("No on moves into the white house!").css({'color': 'red','margin' : '10%', 'margin-top' : '10%', 'display' : 'inline-block', 'width': '40%', 'text-align':'center'});
+        boardMsg("No one moves into the white house!").css({'color': 'red','margin' : '10%', 'margin-top' : '10%', 'display' : 'inline-block', 'width': '40%', 'text-align':'center'});
         winner = 1;
         turn=0;
       }
