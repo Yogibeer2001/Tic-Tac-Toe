84f8ee971bd4e9090f175b65c06c9b58a1efaff4
Thomas Langnickel <Langnickel.thomas@gmail.com>
2016-05-12 20:17:02 +1000

    confetti party added and style fixes

---
+++ README.md
@@ -0,0 +1,10 @@
+*left side screen Menu
+central located game field (start with 3x3 fields
+hover effect with the selectors when hovering over field
+selection of different avatars from menu page
+display of score on right hand side*
+
+#Tic Tac Toe game
+#### using html, css, javascript and jQuery
+
+theme style is inspired by upcoming US elections
--- js/TicTac.js
+++ js/TicTac.js
@@ -82,12 +82,12 @@ function setTurn() {
     if(randomNumber == 1)
     {
       currentPlayer = playerOne.src;
-        turnNote(playerOne.name+"'s turn to start!");
+        turnNote(playerOne.name+"'s turn to start!").css('display', 'center');
     }
     else
     {
       currentPlayer = playerTwo.src;
-        turnNote(playerTwo.name +"'s turn to start!");
+        turnNote(playerTwo.name +"'s turn to start!").css('display', 'center');
     }
     return currentPlayer;
 }
@@ -112,21 +112,174 @@ function start() {
         $('#gameField').css({'display': 'inline-block'});
         setTurn();
 }
-// function computerRandom () {
-//   var cordinateX = (Math.floor((Math.random() * 3)+1));
-//   var cordinateY = (Math.floor((Math.random() * 3)+1));
-//     if(grid[cordinateX][cordinateY] == 0) {
-//       grid[cordinateX][cordinateY] = 2;
-//     } else {
-//         computerRandom();
-//   }
-// }
 
+function confettiShoot()
+{
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");
+    width = canvas.width = window.innerWidth;
+    height = canvas.height = window.innerHeight;
+    particle = [];
+    particleCount = 0,
+      gravity = 0.1,
+      colors = [
+        '#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5',
+        '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4CAF50',
+        '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800',
+        '#FF5722', '#795548'
+      ];
+    for (var i = 0; i < 500; i++) {
+
+      particle.push({
+        x: width / 2,
+        y: height / 2,
+        boxW: randomRange(5, 20),
+        boxH: randomRange(5, 20),
+        size: randomRange(2, 8),
+
+        spikeran: randomRange(3, 5),
+
+        velX: randomRange(-8, 8),
+        velY: randomRange(-50, -10),
+
+        angle: convertToRadians(randomRange(0, 360)),
+        color: colors[Math.floor(Math.random() * colors.length)],
+        anglespin: randomRange(-0.2, 0.2),
+
+        draw: function() {
+
+          context.save();
+          context.translate(this.x, this.y);
+          context.rotate(this.angle);
+          context.fillStyle = this.color;
+          context.beginPath();
+          // drawStar(0, 0, 5, this.boxW, this.boxH);
+          context.fillRect(this.boxW / 2 * -1, this.boxH / 2 * -1, this.boxW, this.boxH);
+          context.fill();
+          context.closePath();
+          context.restore();
+          this.angle += this.anglespin;
+          this.velY *= 0.999;
+          this.velY += 0.3;
+
+          this.x += this.velX;
+          this.y += this.velY;
+          if (this.y < 0) {
+            this.velY *= -0.2;
+            this.velX *= 0.9;
+          };
+          if (this.y > height) {
+            this.anglespin = 0;
+            this.y = height;
+            this.velY *= -0.2;
+            this.velX *= 0.9;
+          };
+          if (this.x > width || this.x < 0) {
+
+            this.velX *= -0.5;
+          };
+
+        },
+
+      });
+
+    }
+    r1 = {
+      x: width / 2 - 150,
+      y: height / 2 - 150,
+      width: 300,
+      height: 300,
+      velX: 0,
+      velY: -10,
+      alphatop: 0
+    };
+    function drawScreen() {
+      size = 50;
+      pFontName = "Lucida Sans Unicode";
+      if (r1.alphatop < 1) {
+        r1.alphatop += 0.01;
+      } else {
+        r1.alphatop = 1;
+      }
+      context.globalAlpha = r1.alphatop;
+
+      if (r1.alphatop === 1) {
+        r1.velY *= 0.999;
+        r1.velY += 0.3;
+
+        r1.x += r1.velX;
+        r1.y += r1.velY;
+      }
+
+      if (r1.y + r1.height > height) {
+        r1.anglespin = 0;
+        r1.y = height - r1.height;
+        r1.velY *= -0.8;
+        r1.velX *= 0.9;
+      };
+
+      context.globalAlpha = 1;
+      for (var i = 0; i < particle.length; i++) {
+        particle[i].draw();
+
+      }
+
+    }
+    function loadImage(url) {
+      var img = document.createElement("img");
+      img.src = url;
+      return img;
+    }
+    function update() {
+
+      context.clearRect(0, 0, width, height);
+
+      drawScreen();
+
+      requestAnimationFrame(update);
+    }
+    update();
+    function randomRange(min, max) {
+      return min + Math.random() * (max - min);
+    }
+    function randomInt(min, max) {
+      return Math.floor(min + Math.random() * (max - min + 1));
+    }
+    function convertToRadians(degree) {
+      return degree * (Math.PI / 180);
+    }
+    function drawStar(cx, cy, spikes, outerRadius, innerRadius, color) {
+      var rot = Math.PI / 2 * 3;
+      var x = cx;
+      var y = cy;
+      var step = Math.PI / spikes;
+
+      context.strokeSyle = "#000";
+      context.beginPath();
+      context.moveTo(cx, cy - outerRadius)
+      for (i = 0; i < spikes; i++) {
+        x = cx + Math.cos(rot) * outerRadius;
+        y = cy + Math.sin(rot) * outerRadius;
+        context.lineTo(x, y)
+        rot += step
+
+        x = cx + Math.cos(rot) * innerRadius;
+        y = cy + Math.sin(rot) * innerRadius;
+        context.lineTo(x, y)
+        rot += step
+      }
+      context.lineTo(cx, cy - outerRadius)
+      context.closePath();
+      context.fillStyle = color;
+      context.fill();
+
+    }
+}
 
 avatar.on('click', function () {
             if (playerOne === '') {
         playerOne = this;
-        avatarOne.append(playerOne).css('display', 'flex');
+        avatarOne.append(playerOne).css('display', 'inline');
         avatarIntro(playerOne);
         scoreBoardOne.text(playerOne.name + ' : ' + playerOneScore);
         console.log(playerOne.name);
@@ -134,11 +287,9 @@ avatar.on('click', function () {
     }
           else if (playerTwo === '') {
               playerTwo = this;
-              avatarTwo.append(playerTwo).css('display', 'flex');
+              avatarTwo.append(playerTwo)/*.css('display', 'inline-flex')*/;
               avatarIntro(playerTwo);
               scoreBoardTwo.text(playerTwo.name + ' : ' + playerTwoScore);
-              console.log(playerTwo.name);
-              console.log(playerTwo.alt);
     } else {
         setTurn();
         $('.menu').css('display', 'none');
@@ -192,24 +343,28 @@ $('.column').on('click', function() {
           $('#gameField').css('display', 'none');
           $('#gameField').css('background-image', 'playerOne');
           boardMsg(playerOne.alt)
-                  .css({'color': 'red',
-                        'display': 'block',
-                        'margin-left' : '15%',
-                        'margin-right' : '15%',
-                        'margin-top' : '0%',
-                        'width': '300px',
-                        'height': '300px',
-                        'font-size': '40px'
-                      })
-                      .append(playerOne);
+          .css({'color': 'red',
+                'display': 'block',
+                'margin-left' : '15%',
+                'margin-right' : '15%',
+                'margin-top' : '0%',
+                'max-width': '350px',
+                'maxheight': '350px',
+                'font-size': '40px'
+                })
+          .append(playerOne);
           $(playerOne)
-              .css({'width': '350px',
-                    'height': '350px'});
+          .css({'width': '350px',
+                'height': '350px'});
           turnNote(playerOne.name + ' wins this round!')
-              .animate({width: [ "toggle", "swing" ],
-    height: [ "toggle", "swing" ],
-    opacity: "toggle"
-  }, 5000, "linear");
+  //             .animate({opacity: 0.65,
+  //                       top: "+=50",
+  //                       left: '-=50px',
+  //                       width: "toggle"
+  // }, 3000);
+  $('#board').append('<canvas id="canvas"></canvas>').css('z-index', '-1');
+  confettiShoot();
+
           avatarSound(playerOne);
           playerOneScore++;
           winSound.play();
@@ -217,7 +372,7 @@ $('.column').on('click', function() {
                     .css({"color": "red",
                           'font-size' : '30px',
                           'font-weight' : 'bold',
-                          'display': 'block'});
+                          'display': 'block'})
         }
         $(this).css('background-image', 'url(' + currentPlayer + ')');
         currentPlayer = playerTwo.src;
@@ -232,25 +387,27 @@ $('.column').on('click', function() {
             $('#gameField').css('background-image', 'playerTwo');
 
             boardMsg(playerTwo.alt)
-                    .css({'color': 'red',
-                          'display': 'block',
-                          'margin-left' : '15%',
-                          'margin-right' : '15%',
-                          'margin-top' : '0%',
-                          'width': '350px',
-                          'height': '350px',
-                          'font-size': '40px'
+              .css({'color': 'red',
+                    'display': 'block',
+                    'margin-left' : '15%',
+                    'margin-right' : '15%',
+                    'margin-top' : '0%',
+                    'max-width': '350px',
+                    'maxheight': '350px',
+                    'font-size': '40px'
                           })
-                    .append(playerTwo);
+              .append(playerTwo);
           $(playerTwo)
-              .css({'width': '400px',
-                    'height': '400px'});
-
+              .css({'width': '300px',
+                    'height': '300px'});
+$('#board').append('<canvas id="canvas"></canvas>').css('z-index', '-1');
+confettiShoot();
             turnNote(playerTwo.name + ' wins this round!')
-            .animate({width: [ "toggle", "swing" ],
-  height: [ "toggle", "swing" ],
-  opacity: "toggle"
-}, 5000, "linear");
+//             .animate({opacity: 0.65,
+//                       top: "+=50",
+//                       left: '-=50px',
+//                       width: "toggle"
+// }, 3000);
             avatarSound(playerTwo);
 
             playerTwoScore++;
@@ -272,11 +429,12 @@ $('.column').on('click', function() {
                 'margin-top' : '10%',
                 'display' : 'inline-block',
                 'width': '40%',
-                'text-align':'center'});
-          $('.board').animate({width: [ "toggle", "swing" ],
-      height: [ "toggle", "swing" ],
-      opacity: "toggle"
-      }, 5000, "linear");
+                'text-align':'center'})
+    //             .animate({opacity: 0.85,
+    //                       top: "-=50",
+    //                       left: '+=50px',
+    //                       width: "toggle"
+    // }, 5000);
         noWinner.play();
         winner = 1;
         turn=0;
